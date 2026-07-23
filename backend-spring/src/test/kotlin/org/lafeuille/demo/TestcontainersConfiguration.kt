2b0928330992b5ebfa49663c2d53c4b06e4dd838
Olivier Le Merdy <1182046+olemerdy@users.noreply.github.com>
2026-07-23 18:02:42 +0200

    Mount volume for Ollama models

--- backend-spring/src/test/kotlin/org/lafeuille/demo/TestcontainersConfiguration.kt
+++ backend-spring/src/test/kotlin/org/lafeuille/demo/TestcontainersConfiguration.kt
@@ -3,11 +3,26 @@ package org.lafeuille.demo
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
+import org.testcontainers.containers.BindMode
+import org.testcontainers.containers.SelinuxContext
 import org.testcontainers.ollama.OllamaContainer
+import java.io.File
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
-    fun ollamaContainer(): OllamaContainer = OllamaContainer("ollama/ollama")
+    fun ollamaContainer(): OllamaContainer {
+        val hostPath = System.getProperty("user.home") + "/.ollama"
+        File(hostPath).mkdirs()
+
+        val container = OllamaContainer("ollama/ollama")
+        container.addFileSystemBind(
+            hostPath,
+            "/root/.ollama",
+            BindMode.READ_WRITE,
+            SelinuxContext.SHARED,
+        )
+        return container
+    }
 }
