9654798abf829626dda0291b042d1f916bf37016
Olivier Le Merdy <1182046+olemerdy@users.noreply.github.com>
2026-08-05 15:22:48 +0200

    Use graphic card on Steam Deck

--- backend-spring/src/test/kotlin/org/lafeuille/demo/TestcontainersConfiguration.kt
+++ backend-spring/src/test/kotlin/org/lafeuille/demo/TestcontainersConfiguration.kt
@@ -1,5 +1,6 @@
 package org.lafeuille.demo
 
+import com.github.dockerjava.api.model.Device
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
@@ -17,6 +18,12 @@ class TestcontainersConfiguration {
         File(hostPath).mkdirs()
 
         val container = OllamaContainer("ollama/ollama")
+            .withEnv("OLLAMA_IGPU_ENABLE", "" + true)
+            .withEnv("HSA_OVERRIDE_GFX_VERSION", "10.3.0")
+            .withCreateContainerCmdModifier { it.hostConfig?.withDevices(
+                Device("rwm", "/dev/kfd", "/dev/kfd"),
+                Device("rwm", "/dev/dri", "/dev/dri")
+            ) }
         container.addFileSystemBind(
             hostPath,
             "/root/.ollama/models",
